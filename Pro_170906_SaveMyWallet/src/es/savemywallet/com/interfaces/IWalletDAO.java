c96fae0f56c3c8d09506ccee28d14a843718effb
TamaraRodriguez <tamara90decor@gmail.com>
2017-09-06 16:59:38 +0200

    cambios en I

--- Pro_170906_SaveMyWallet/src/es/savemywallet/com/interfaces/IWalletDAO.java
+++ Pro_170906_SaveMyWallet/src/es/savemywallet/com/interfaces/IWalletDAO.java
@@ -6,6 +6,8 @@ import es.savemywallet.com.beans.User;
 
 public interface IWalletDAO {
 	
+	public User findByPrimaryId(int idWallet);
+	public void delete(int idWallet);
 	public int add(User user);
 	public void update(User user);
 	public List<User> list();
