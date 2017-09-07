d0c07865ace1f3547f9b4f5cf5447f4e3c5be4b9
hugo1988 <hugomachadoperez@outlook.com>
2017-09-07 12:53:32 +0200

    Añado modificaciones
    Pequeñas actualizaciones es ficheros---> ConceptService.java,
    ConceptService.java, MovementService.java

--- Pro_170906_SaveMyWallet/src/es/savemywallet/com/services/ConceptService.java
+++ Pro_170906_SaveMyWallet/src/es/savemywallet/com/services/ConceptService.java
@@ -6,9 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import es.savemywallet.com.beans.Concept;
-import es.savemywallet.com.beans.Movement;
 import es.savemywallet.com.daos.ConceptDAO;
-import es.savemywallet.com.daos.MovementDAO;
 import es.savemywallet.com.interfaces.IConceptService;
 
 public class ConceptService implements IConceptService {
@@ -27,31 +25,42 @@ public class ConceptService implements IConceptService {
 		conceptDAO = (ConceptDAO) context.getBean("conceptDAO");		
 	}
 	
+	/**
+	 * Method fin by primary id_concept
+	 */
 	@Override
-	public Concept findByPrimaryIdMovement(int idConcept) {
+	public Concept findByPrimaryIdConcept(int idConcept) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-
+	/**
+	 * Method add concept
+	 */
 	@Override
 	public void addConcept(Concept concept) {
 		conceptDAO.add(concept);		
-
 	}
-
+	
+	/**
+	 * Method update concept
+	 */
 	@Override
 	public void updateConcept(Concept concept) {
 		conceptDAO.update(concept);		
-
 	}
-
+	
+	/**
+	 * Method delete id_concept
+	 */
 	@Override
 	public void deleteConcept(int idConcept) {
 		conceptDAO.delete(idConcept);		
-	
 	}
 
+	/**
+	 * Method list concept
+	 */
 	@Override
 	public List<Concept> listConcept() {
 		List<Concept> listConcept = conceptDAO.list();
